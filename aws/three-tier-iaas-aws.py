69064a86a9e2c9abc3777f7317d3e9091260a5f5
byFrederick <freddycruz5227@gmail.com>
2023-04-08 16:41:57 -0400

    AWS Three-tier classis arch updated

--- aws/three-tier-iaas-aws.py
+++ aws/three-tier-iaas-aws.py
@@ -1,17 +1,18 @@
 from diagrams import Diagram, Cluster
-from diagrams.aws.network import VPC
-from diagrams.aws.network import PublicSubnet
-from diagrams.aws.network import PrivateSubnet           
+from diagrams.aws.network import VPC     
 from diagrams.aws.compute import EC2               
-from diagrams.aws.database import Aurora
+from diagrams.aws.database import RDS
 from diagrams.aws.network import ALB 
-from diagrams.aws.network import ClientVpn          
+from diagrams.aws.network import ClientVpn  
+from diagrams.aws.general import Users    
+from diagrams.aws.general import InternetAlt2    
 
 
 web_app_name = "Three tier web application"
 with Diagram(web_app_name, show=False):
+    clients = Users("Clients")
+    internet = InternetAlt2("Internet")
     with Cluster("VPC"):
-        VPC_Global = VPC("VPC")
         ALBFI = ALB("ALB (Facing internet)")
         with Cluster ("Private Subnet"):
             ALBFP = ALB("ALB (Facing Private)")
@@ -20,5 +21,6 @@ with Diagram(web_app_name, show=False):
             with Cluster ("ASG App Tier"):
                 EC2_App_Tier = [EC2("App Tier"), EC2("App Tier"), EC2("App Tier")]
             with Cluster("Database Tier"):
-                DB = Aurora("Amazon Aurora")
-    ALBFI >> EC2_Web_Tier >> ALBFP >> EC2_App_Tier >> DB
+                DB = RDS("RDS")
+    
+    clients >> internet >> ALBFI >> EC2_Web_Tier >> ALBFP >> EC2_App_Tier >> DB
